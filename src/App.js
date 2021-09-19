f99e9098cff65f10498ac2bd0f997ff2455c1a2c
Shrish Sharma <shrish0608@gmail.com>
2021-09-19 19:58:04 +0530

    Landing Page Added (Boilerplate)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import Landing from './pages/Landing';
 
 function App() {
   return (
       <Router>
           <Switch>
+            <Route exact path='/' component={Landing} />
             <Route path='/Login' component={Login} />
             <Route path='/Signup' component={Signup} />
           </Switch>
---
+++ src/pages/Landing.js
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+import React from 'react'
+
+const Landing = () => {
+    return ( 
+        <Typography>
+            LMFAOOOOOOOOOOO
+            FEED is HERE.
+        </Typography>
+    );
+}
+ 
+export default Landing;
