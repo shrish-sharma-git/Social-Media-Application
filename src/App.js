6776007f82304c35fab9c92857849ab0edf8b612
Shrish Sharma <shrish0608@gmail.com>
2021-09-21 17:02:03 +0530

    changed Landing to Home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import Landing from './pages/Landing';
+import Home from './pages/Home';
 
 function App() {
   return (
       <Router>
           <Switch>
-            <Route exact path='/' component={Landing} />
+            <Route exact path='/' component={Home} />
             <Route path='/Login' component={Login} />
             <Route path='/Signup' component={Signup} />
           </Switch>
