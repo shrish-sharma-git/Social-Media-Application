34d472a7b616c184a5d0441654294a6327f20bf1
Shrish Sharma <shrish0608@gmail.com>
2021-09-24 21:58:10 +0530

    Profile Page UI

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,54 +1,77 @@
-import { FavoriteRounded } from '@mui/icons-material';
-import { AppBar, Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material';
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Container, Divider, Grid, IconButton, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 import MenuDrawer from './components/MenuDrawer';
-import RightSidebar from './components/RightSidebar';
 
 const Profile = () => {
     return (  
         <Box>
             <MenuDrawer />
-            <AppBar
-                sx={{width: '100%'}}
-            />
-            <Box component="main" sx={{ flexGrow: 1, p: 3, margin: {xs: '50px 0px'}, marginLeft: {sm: '240px'}, marginRight: {sm: '400px'}  }}>
+            <Box component="main" sx={{ flexGrow: 1, p: 3, margin: {xs: '50px 0px'}, marginLeft: {sm: '240px'}, marginRight: {sm: '400px'} }}>
+                
+                    <Grid container>    
+                        <Grid item xs={6}> 
+                            <Avatar
+                                src="https://i.ibb.co/bBWdJJb/452ea912ad7d9a12e707b9aab4deba3e.jpg"
+                                sx={{ height: 110, width: 110 }}
+                            />
+                            <Typography
+                                variant="h6"
+                            >
+                                Marshall Mathers
+                            </Typography>
+                            <Typography
+                                variant='caption'
+                                sx={{fontSize: 16}}
+                            >
+                                @slim_shady
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={3} sx={{ textAlign: 'center', mt: '25px' }}>
+                            <Typography
+                                variant="subtitle2"
+                            >
+                                Followers  
+                            </Typography>
+                            <Typography
+                                variant="h6"
+                            >
+                                10m  
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={3} sx={{ textAlign: 'center', mt: '25px'  }}>
+                            <Typography
+                                variant="subtitle2"
+                            >
+                                Following  
+                            </Typography>
+                            <Typography
+                                variant="h6"
+                            >
+                                69   
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Divider sx={{m: '10px 0 10px 0'}} />
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Button
+                                variant="contained"
+                                fullWidth
+                            >
+                                Follow
+                            </Button>
+                        </Grid>    
+                    </Grid>
 
-            <Card sx={{ margin: {xs: '15px 0px 15px'}, padding: {xs: '15px'}  }}>
-                <CardHeader
-                avatar={
-                    <Avatar sx={{ backgroundColor: 'red' }} aria-label="recipe">
-                    R
-                    </Avatar>
-                }
-                title="Dr. Dre"
-                subheader="September 14, 2016"
-                />
-                <CardMedia
-                component="img"
-                height="194"
-                image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-                alt="Post"
-                />
-                <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                    This impressive paella is a perfect party dish and a fun meal to cook
-                    together with your guests. Add 1 cup of frozen peas along with the mussels,
-                    if you like.
-                </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteRounded />
-                </IconButton>
-                <Typography
-                    variant="subtitle2"
-                    color="gray"
-                >
-                    1k
-                </Typography>
-                </CardActions>
-            </Card>
+                    <Grid container>
+                        <Grid item xs={6}>
+                            
+                        </Grid>
+                        <Grid item xs={6}>
+
+                        </Grid>
+                    </Grid>
             </Box>
         </Box>
     );
