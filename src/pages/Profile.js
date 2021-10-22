6d14e6f50f3f3268b88dcf63d0b2be43e4edc151
Shrish Sharma <shrish0608@gmail.com>
2021-10-22 21:37:39 +0530

    Added Posts Modal - Profile Section

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,14 @@
 import { InfoRounded } from '@mui/icons-material';
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Container, Divider, Grid, IconButton, ImageList, ImageListItem, ImageListItemBar, ListSubheader, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import React, { useState } from 'react';
 import MenuDrawer from './components/MenuDrawer';
+import PostModal from './components/PostModal';
 
 const Profile = () => {
+    // Post Modal State Handling
+    const [selectedImg, setSelectedImage] = useState(null);
+    
     return (  
         <Box>
             <MenuDrawer />
@@ -83,6 +87,7 @@ const Profile = () => {
                         {itemData.map((item) => (
                             <ImageListItem key={item.img}>
                             <img
+                                onClick={() => setSelectedImage(item.img)}
                                 src={`${item.img}?w=248&fit=crop&auto=format`}
                                 srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                                 alt={item.title}
@@ -91,18 +96,19 @@ const Profile = () => {
                             <ImageListItemBar
                                 title={item.title}
                                 subtitle={item.author}
-                                actionIcon={
-                                <IconButton
-                                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                                    aria-label={`info about ${item.title}`}
-                                >
-                                    <InfoRounded />
-                                </IconButton>
-                                }
+                                // actionIcon={
+                                // <IconButton
+                                //     sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                //     aria-label={`info about ${item.title}`}
+                                // >
+                                //     <InfoRounded />
+                                // </IconButton>
+                                // }
                             />
                             </ImageListItem>
                         ))}
                     </ImageList>
+                    { selectedImg && <PostModal selectedImg={selectedImg} setSelectedImage={setSelectedImage}/>}
             </Box>
         </Box>
     );
@@ -113,7 +119,7 @@ const itemData = [
     {
       img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
       title: 'Breakfast',
-      author: '@bkristastucchio',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
       rows: 2,
       cols: 2,
       featured: true,
@@ -121,29 +127,29 @@ const itemData = [
     {
       img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
       title: 'Burger',
-      author: '@rollelflex_graphy726',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
     },
     {
       img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
       title: 'Camera',
-      author: '@helloimnik',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
     },
     {
       img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
       title: 'Coffee',
-      author: '@nolanissac',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
       title: 'Hats',
-      author: '@hjrc33',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
       title: 'Honey',
-      author: '@arwinneil',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
       rows: 2,
       cols: 2,
       featured: true,
@@ -151,34 +157,34 @@ const itemData = [
     {
       img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
       title: 'Basketball',
-      author: '@tjdragotta',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
     },
     {
       img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
       title: 'Fern',
-      author: '@katie_wasserman',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
     },
     {
       img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
       title: 'Mushrooms',
-      author: '@silverdalex',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
       rows: 2,
       cols: 2,
     },
     {
       img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
       title: 'Tomato basil',
-      author: '@shelleypauls',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
     },
     {
       img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
       title: 'Sea star',
-      author: '@peterlaster',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
     },
     {
       img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
       title: 'Bike',
-      author: '@southside_customs',
+      author: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, culpa?',
       cols: 2,
     },
 ];
---
+++ src/pages/components/PostModal.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const PostModal = ({selectedImg, setSelectedImage }) => {
+    const handleClick = (e) => {
+        console.log('Image Clicked')
+        if (e.target.classList.contains('backdrop')) {
+            setSelectedImage(null);
+        }
+    }
+
+    return (  
+        <div className="backdrop" onClick={handleClick}>
+            <img 
+                src={selectedImg}
+                alt="Post Image"
+            />
+        </div>
+    );
+}
+ 
+export default PostModal;
