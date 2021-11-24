33daf83ee59e0e5342b888134726eb3ea3a95a2f
Shrish Sharma <shrish0608@gmail.com>
2021-11-24 22:39:09 +0530

    Update Email and Password in Settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,6 +1,6 @@
-import { Avatar, Button, Card, CardHeader, Divider, Grid, IconButton, Typography } from '@mui/material';
+import { Avatar, Backdrop, Button, Card, CardHeader, Divider, Fade, Grid, IconButton, Modal, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import MenuDrawer from './components/MenuDrawer';
 import EditRoundedIcon from '@mui/icons-material/EditRounded';
 import { useHistory } from 'react-router';
@@ -8,7 +8,61 @@ import { useAuth } from '../context/AuthContext';
 
 const Settings = () => {
     const [error, setError] = useState('');
-    const { currentUser, logout } = useAuth();
+    const { currentUser, logout, updateEmail, updatePassword } = useAuth();
+  
+    // Modal (Email & Password Change)
+    function handleEmailPasswordChangeSubmit(e) {
+        e.preventDefault();
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError("Passwords do not match")
+        }
+    
+        const promises = []
+        setLoading(true)
+        setError("")
+    
+        if (emailRef.current.value !== currentUser.email) {
+        promises.push(updateEmail(emailRef.current.value))
+        }
+        if (passwordRef.current.value) {
+        promises.push(updatePassword(passwordRef.current.value))
+        }
+    
+        Promise.all(promises)
+        .then(() => {
+            history.push("/")
+        })
+        .catch(() => {
+            setError("Failed to update account")
+        })
+        .finally(() => {
+            setLoading(false)
+        })
+    }
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+
+    const [loading, setLoading] = useState(false)
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'white',
+        color: 'black',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
 
     const history = useHistory();
 
@@ -116,7 +170,9 @@ const Settings = () => {
                             subheader="Privacy & Security Settings"
                             action={
                                 <IconButton aria-label="edit">
-                                    <EditRoundedIcon/>
+                                    <EditRoundedIcon
+                                        onClick={handleOpen}
+                                    />
                                 </IconButton>
                             }
                     />
@@ -146,6 +202,67 @@ const Settings = () => {
                         </Grid>
                     </Grid>
                 </Card>
+                {/* MODAL Email & Password Change MODAL */}
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                      timeout: 500,
+                    }}
+                >
+                    <Fade in={open}>
+                        <Box sx={style}>
+                        <Typography id="transition-modal-title" variant="h6" component="h2">
+                            Edit Email and Pass
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleEmailPasswordChangeSubmit} sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                inputRef={emailRef}
+                                id="email"
+                                label="Email"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                inputRef={passwordRef}
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <TextField
+                            required
+                            inputRef={passwordConfirmRef}
+                            fullWidth
+                            name="confirm password"
+                            label="Confirm Password"
+                            type="password"
+                            id="confirm-password"
+                            autoComplete="new-password"
+                            />
+                            <Button
+                                type="submit"
+                                disabled={loading}
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Save Changes
+                            </Button>
+                            </Box>
+                        </Box>
+                    </Fade>
+                </Modal>
 
                 <Divider sx={{ m: '25px 0 25px 0' }}/>                
 
